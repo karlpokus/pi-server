1ac09d36b0f5747e7acbd5570cf22d313eb4979c
Karl Pokus <karl.pokus@gmail.com>
2017-02-12 10:37:04 +0100

    add uptime

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ var http = require('http'),
     server = http.createServer(),
     PORT = process.env.PORT || 3000,
     os = require('os'),
-    name = os.hostname();
+    name = os.hostname(),
+    roundToDecimal = function(num, dec){
+        return +num.toFixed(dec);
+    };
 
 server
     .on('request', function(res, res){
         var total = os.totalmem(),
             usage = total - os.freemem(),
-            perc = +(usage / total *100).toFixed(2); 
-        res.end(`I am ${name}. Using ${perc}% memory right now. O_o`);
+            perc = roundToDecimal(usage / total *100, 2),
+            awakeHrs = roundToDecimal(os.uptime() / (60 * 60), 2);
+
+        res.end(`I am ${name}. I have been up for ${awakeHrs} hrs using ${perc}% memory right now. O_o`);
     })
     .listen(PORT, function(){
         console.log('pi-server running..');
